6835a7331b3d80c3687c299c1ac79231b810555e
Karthikeyan <karthikeyanp.98@gmail.com>
2019-01-11 12:28:35 +0530

    basic build

--- app/src/main/java/com/example/karthik/demoapplication/MainActivity.java
+++ app/src/main/java/com/example/karthik/demoapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.karthik.demoapplication;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +11,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     DataAdapter dataAdapterTwo;
     List<Movies> dataOne;
     List<Movies> dataTwo;
+    int selectedPosition = -1;
+
+    FloatingActionButton FAB1, FAB2, FAB3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +45,31 @@ public class MainActivity extends AppCompatActivity {
         recyclerViewTwo.setAdapter(dataAdapterTwo);
         recyclerViewOne.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true));
         recyclerViewTwo.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true));
+
+        FAB1 = findViewById(R.id.viewFAB);
+        FAB2 = findViewById(R.id.backFAB);
+        FAB3 = findViewById(R.id.nextFAB);
+
+        FAB1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Video Library", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        FAB2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Back", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        FAB3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "New Activity", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public class DataAdapter extends RecyclerView.Adapter<DataAdapter.mViewHolder> {
@@ -52,12 +85,37 @@ public class MainActivity extends AppCompatActivity {
         @NonNull
         @Override
         public DataAdapter.mViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+            View view = inflater.inflate(R.layout.horizontal_list, parent, false);
+            DataAdapter.mViewHolder holder = new DataAdapter.mViewHolder(view);
+            return holder;
         }
 
         @Override
-        public void onBindViewHolder(@NonNull DataAdapter.mViewHolder holder, int position) {
+        public void onBindViewHolder(@NonNull DataAdapter.mViewHolder holder, final int position) {
+            final Movies current = data.get(position);
+            holder.listImage.setImageResource(current.resID);
+            holder.listText.setText(current.movieName);
 
+            if(selectedPosition == position)
+                holder.itemView.setBackgroundColor(Color.parseColor("#D4D7F2"));
+            else
+                holder.itemView.setBackgroundColor(Color.parseColor("#FFFFFF"));
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getApplicationContext(), current.movieName, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    selectedPosition = position;
+                    notifyDataSetChanged();
+                    return true;
+                }
+            });
         }
 
         @Override
@@ -66,20 +124,48 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public class mViewHolder extends RecyclerView.ViewHolder {
+            ImageView listImage;
+            TextView listText;
 
             public mViewHolder(View itemView) {
                 super(itemView);
+                listImage = itemView.findViewById(R.id.imageViewList);
+                listText = itemView.findViewById(R.id.textViewList);
             }
         }
     }
 
     public class Movies {
+        private int resID;
+        private String movieName;
 
+        Movies(int resID, String movieName)
+        {
+            this.resID = resID;
+            this.movieName = movieName;
+        }
     }
 
     public List<Movies> getListOne()
     {
         dataOne = new ArrayList<>();
+
+        Movies movies;
+
+        movies = new Movies(R.drawable.interstellar, "Interstellar");
+        dataOne.add(movies);
+
+        movies = new Movies(R.drawable.godfather, "Godfather");
+        dataOne.add(movies);
+
+        movies = new Movies(R.drawable.rosemarys_baby, "Rosemary's Baby");
+        dataOne.add(movies);
+
+        movies = new Movies(R.drawable.ib, "Inglourious Basterds");
+        dataOne.add(movies);
+
+        movies = new Movies(R.drawable.lobster, "Lobster");
+        dataOne.add(movies);
 
         return dataOne;
     }
@@ -87,6 +173,17 @@ public class MainActivity extends AppCompatActivity {
     public List<Movies> getListTwo()
     {
         dataTwo = new ArrayList<>();
+
+        Movies movies;
+
+        movies = new Movies(R.drawable.shawshank, "Shawshank Redemption");
+        dataTwo.add(movies);
+
+        movies = new Movies(R.drawable.annihilation, "Annihilation");
+        dataTwo.add(movies);
+
+        movies = new Movies(R.drawable.twelve_angry_men, "12 Angry Men");
+        dataTwo.add(movies);
 
         return  dataTwo;
     }
